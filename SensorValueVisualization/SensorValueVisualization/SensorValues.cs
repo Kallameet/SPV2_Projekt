1115e21519460122c3086a611fa7eff7921e54fa
Djon <bernhard.selymes@gmail.com>
2015-05-20 23:12:51 +0200

    Sensor Visualization with Windows Client
    only crashes if you close it in the right moment :confused:

---
+++ SensorValueVisualization/SensorValueVisualization/SensorValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SensorValuesServer
+{
+    [Serializable]
+    public class SensorValues
+    {
+        public int AccelerometerX { get; set; }
+
+        public int AccelerometerY { get; set; }
+
+        public int AccelerometerZ { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("AccelerometerX: {0}, AccelerometerY: {1}, AccelerometerZ: {2}", AccelerometerX,
+                AccelerometerY, AccelerometerZ);
+        }
+    }
+}
--- SensorValueVisualization/SensorValueVisualization/ViewModel/MainViewModel.cs
+++ SensorValueVisualization/SensorValueVisualization/ViewModel/MainViewModel.cs
@@ -1,4 +1,10 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using SensorValuesServer;
 
 namespace SensorValueVisualization.ViewModel
 {
@@ -16,19 +22,125 @@ namespace SensorValueVisualization.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        private const string IpAdress = "127.0.0.1";
+        private const int Port = 1234;
+        private readonly ChatServer _chatServer;
+        private readonly BackgroundWorker _chatServerWorker;
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            _chatServerWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+            _chatServer = new ChatServer(IpAdress, Port, _chatServerWorker);
+            try
+            {
+                _chatServerWorker.DoWork += RunChatServer;
+                _chatServerWorker.ProgressChanged += ReadSensorValues;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Unexpected Exception occured.");
+                Debug.WriteLine(e.ToString());
+            }
+        }
+
+        ~MainViewModel()
+        {
+            _chatServer.Stop();
+            IsConnected = false;
+        }
+
+        private void RunChatServer(object sender, DoWorkEventArgs e)
+        {
+            _chatServer.Start();
+        }
+
+        private void ReadSensorValues(object sender, ProgressChangedEventArgs e)
+        {
+            SensorValues sensorValues = e.UserState as SensorValues;
+
+            if (sensorValues != null)
+            {
+                AccelerometerX = sensorValues.AccelerometerX;
+                AccelerometerY = sensorValues.AccelerometerY;
+                AccelerometerZ = sensorValues.AccelerometerZ;
+            }
+        }
+
+        private int _accelerometerX;
+        public int AccelerometerX
+        {
+            get { return _accelerometerX; }
+            set
+            {
+                _accelerometerX = value;
+                RaisePropertyChanged(() => AccelerometerX);
+            }
+        }
+
+        private int _accelerometerY;
+        public int AccelerometerY
+        {
+            get { return _accelerometerY; }
+            set
+            {
+                _accelerometerY = value;
+                RaisePropertyChanged(() => AccelerometerY);
+            }
+        }
+
+        private int _accelerometerZ;
+        public int AccelerometerZ
+        {
+            get { return _accelerometerZ; }
+            set
+            {
+                _accelerometerZ = value;
+                RaisePropertyChanged(() => AccelerometerZ);
+            }
+        }
+
+        private RelayCommand _clickStartCommand;
+
+        public RelayCommand ClickStartCommand
+        {
+            get { return _clickStartCommand ?? (_clickStartCommand = new RelayCommand(OnClickStart)); }
+        }
+
+        private void OnClickStart()
+        {
+            if (!_chatServerWorker.IsBusy)
+            {
+                _chatServerWorker.RunWorkerAsync();
+                IsConnected = true;
+            }
+        }
+
+        private RelayCommand _clickStopCommand;
+
+        public RelayCommand ClickStopCommand
+        {
+            get { return _clickStopCommand ?? (_clickStopCommand = new RelayCommand(OnClickStop)); }
+        }
+
+        private void OnClickStop()
+        {
+            _chatServer.Stop();
+            IsConnected = false;
+        }
+
+        private bool _isConnected;
+
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set
+            {
+                _isConnected = value;
+                RaisePropertyChanged(() => IsConnected);
+            }
         }
     }
 }
